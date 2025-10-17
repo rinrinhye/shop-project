4c8e135711110521aa1b0dcd826c972cb842ef3f
rinrinhye <hyerin98323@gmail.com>
2025-10-17 16:18:42 +0900

    4차 스터디 - 2주차

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,97 +1,107 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import type { RegisterInput } from "../types/common";
 import { ROUTES } from "../routes/routes";
 import { useEmailAvailable, useRegister } from "../queries/useAuth";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-	const [value, setValue] = useState<RegisterInput>({
-		name: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-		role: "customer",
-		avatar: "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
-	});
-	const { name, email, password, confirmPassword, avatar } = value;
-	// const [passwordDirty, setPasswordDirty] = useState(false);
-	const [errorMessages] = useState({ name: "", email: "", password: "", confirmPassword: "" });
+	// const [value, setValue] = useState<RegisterInput>({
+	// 	name: "",
+	// 	email: "",
+	// 	password: "",
+	// 	confirmPassword: "",
+	// 	role: "customer",
+	// 	avatar: "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+	// });
+	// const { name, email, password, confirmPassword, avatar } = value;
 
 	const { mutate } = useRegister();
+	const { mutate: checkEmailAvailable, data: isEmailAvailable, isError: isEmailError } = useEmailAvailable();
+
 	const {
-		mutate: checkEmailAvailable,
-		// isError: isEmailAvailableError,
-		// error: emailAvailableError,
-	} = useEmailAvailable();
+		register,
+		getValues,
+		handleSubmit,
+		setError,
+		watch,
+		clearErrors,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+			role: "customer",
+			avatar: "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+		},
+	});
 
-	// useEffect(() => {
-	/* 
-		
-		상황	showMismatch 여부
-    password, confirm 둘 다 안 건드림	    ❌ 숨김
-    confirm만 건드림 (password 비어있음)	 ❌ 숨김
-    password 입력 후 confirm 건드림	      ✅ 비교 시작
-    password 수정하면	confirm 초기화,      ❌ 숨김
-
-		setErrorMessages((prev) => ({...prev, confirmPassword: "비밀번호가 변경되어 확인이 초기화되었어요."}));
-	setErrorMessages((prev) => ({...prev, confirmPassword: "비밀번호가 일치하지 않습니다."}));
-			setErrorMessages((prev) => ({...prev, confirmPassword: ""}));
-
-		*/
-	// }, [password, confirmPassword, passwordDirty]);
-
-	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const name = e.target.name;
-		const value = e.target.value;
-
-		setValue((prev) => {
-			return { ...prev, [name]: value };
-		});
+	const onSubmit = (data: RegisterInput) => {
+		console.log(data);
+		// const payload = { name, email, password, avatar };
+		// mutate(payload);
 	};
 
-	const register = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+	const checkEmail = () => {
+		if (errors.email) {
+			console.log(errors);
+		}
 
-		const payload = { name, email, password, avatar };
-		mutate(payload);
-	};
-
-	const handleCheckEmail = (email: RegisterInput["email"]) => {
+		const email = getValues("email");
 		checkEmailAvailable(email);
 	};
+
+	useEffect(() => {
+		// 400 : 이메일 형식 지켜라
+		// 201 : isAvailable false truue
+
+		if (isEmailError) {
+			setError("email", { type: "custom", message: "중복된 이메일 입니다." });
+		} else clearErrors("email");
+	}, [isEmailError]);
+
+	console.log(errors);
+
+	console.log(useForm());
 
 	return (
 		<div className='main mt-20 text-center'>
 			<h2 className='mb-12 text-3xl font-[Outfit] font-bold'>JOIN</h2>
-			<form action='' onSubmit={register} method='POST' noValidate>
+			<form action='' onSubmit={handleSubmit(onSubmit)} method='POST' noValidate>
 				<div className='flex flex-col items-center gap-5 mb-12'>
 					<div className='form-join'>
 						<label htmlFor='name'>name</label>
 						<input
 							type='name'
-							value={name}
-							onChange={handleInput}
-							name='name'
 							id='name'
 							placeholder=' '
-							className='peer'></input>
+							className='peer'
+							{...register("name", { required: "이름을 입력해 주세요." })}></input>
+						{errors.name && <p>{errors.name.message}</p>}
 					</div>
 					<div className=''>
 						<div className='form-join'>
 							<label htmlFor='email'>email</label>
 							<input
 								type='email'
-								value={email}
-								onChange={handleInput}
-								name='email'
 								id='email'
 								placeholder=' '
-								className='peer'></input>
+								className='peer'
+								{...register("email", {
+									required: "이메일을 입력해 주세요.",
+									pattern: {
+										value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+										message: "올바른 이메일 형식이 아닙니다.",
+									},
+								})}></input>
+							{errors.email && <p>{errors.email.message}</p>}
 						</div>
 						<button
 							type='button'
-							onClick={() => handleCheckEmail(email)}
-							className='block button button-sm text-sm ml-auto mr-0 mt-2'>
+							className='block button button-sm text-sm ml-auto mr-0 mt-2'
+							onClick={checkEmail}>
 							check email
 						</button>
 					</div>
@@ -99,24 +109,24 @@ const Register = () => {
 						<label htmlFor='password'>password</label>
 						<input
 							type='password'
-							value={password}
-							onChange={handleInput}
-							name='password'
 							id='password'
 							placeholder=' '
-							className='peer'></input>
+							className='peer'
+							{...register("password", {
+								required: "비밀번호를 입력해 주세요.",
+								minLength: { value: 6, message: "6자 이상 입력해 주세요." },
+							})}></input>
+						{errors.password && <p>{errors.password.message}</p>}
 					</div>
 					<div className='form-join'>
 						<label htmlFor='confirmPassword'>confirm password</label>
 						<input
 							type='password'
-							value={confirmPassword}
-							onChange={handleInput}
-							name='confirmPassword'
 							id='confirmPassword'
 							placeholder=' '
-							className='peer'></input>
-						{errorMessages.confirmPassword !== "" && <p>{errorMessages.confirmPassword}</p>}
+							className='peer'
+							{...register("confirmPassword", { required: "비밀번호 확인란을 입력해 주세요." })}></input>
+						{errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
 					</div>
 				</div>
 				<button type='submit' className='button button-xl button-black w-84 font-bold'>
