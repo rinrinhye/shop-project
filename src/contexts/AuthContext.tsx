e4029d52b3acfbf3f141407b0dfa129a7671cc85
rinrinhye <hyerin98323@gmail.com>
2025-11-03 10:55:52 +0900

    4차 스터디 - 4주차 : 로그아웃 시 홈 이동

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,43 +1,45 @@
 import { createContext, useContext, useMemo, useState, useEffect } from "react";
 import { useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
+import { ROUTES } from "../routes/routes";
 
 type AuthCtx = {
-  token: string | null;
-  login: (token: string) => void;
-  logout: () => void;
+	token: string | null;
+	login: (token: string) => void;
+	logout: () => void;
 };
 
 const AuthContext = createContext<AuthCtx | null>(null);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const qc = useQueryClient();
-  const [token, setToken] = useState<string | null>(() =>
-    localStorage.getItem("access_token")
-  );
+	const qc = useQueryClient();
+	const navigate = useNavigate();
+	const [token, setToken] = useState<string | null>(() => localStorage.getItem("access_token"));
 
-  // 토큰을 단일 소스로 관리(상태 → localStorage 동기화)
-  useEffect(() => {
-    if (token) localStorage.setItem("access_token", token);
-    else localStorage.removeItem("access_token");
-  }, [token]);
+	// 토큰을 단일 소스로 관리(상태 → localStorage 동기화)
+	useEffect(() => {
+		if (token) localStorage.setItem("access_token", token);
+		else localStorage.removeItem("access_token");
+	}, [token]);
 
-  const login = (newToken: string) => {
-    setToken(newToken); // 리렌더 트리거 → useCurrentUser 실행
-  };
+	const login = (newToken: string) => {
+		setToken(newToken); // 리렌더 트리거 → useCurrentUser 실행
+	};
 
-  const logout = async () => {
-    setToken(null); // 리렌더 트리거
-    qc.setQueryData(["currentUser"], null);
-    qc.removeQueries({ queryKey: ["currentUser"] });
-  };
+	const logout = async () => {
+		setToken(null); // 리렌더 트리거
+		qc.setQueryData(["currentUser"], null);
+		qc.removeQueries({ queryKey: ["currentUser"] });
+		navigate(ROUTES.home);
+	};
 
-  const value = useMemo(() => ({ token, login, logout }), [token]);
+	const value = useMemo(() => ({ token, login, logout }), [token]);
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export const useAuth = () => {
-  const ctx = useContext(AuthContext);
-  if (!ctx) throw new Error("useAuth must be used within <AuthProvider>");
-  return ctx;
+	const ctx = useContext(AuthContext);
+	if (!ctx) throw new Error("useAuth must be used within <AuthProvider>");
+	return ctx;
 };
